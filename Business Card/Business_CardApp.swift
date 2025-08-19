d304043f562d052a2b4b3f2047361068cca1f19c
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-08-19 16:33:39 -0400

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Lidiia Diachkovskaia on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
